7b36da92990bdc76c40e8d4abcf86ea8c88630d9
Emmet Delaney <emmetdel@gmail.com>
2016-05-05 16:07:40 +0100

    test

--- admin/admin-feature-box.php
+++ admin/admin-feature-box.php
@@ -24,6 +24,7 @@
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Feature Boxes</a></li>
+      ..
       <li><a href="admin-membership.php">Membership</a></li>
       <li><a href="admin-classes.php">Classes</a></li>
       <li><a href="admin-testimonials.php">Testimonials</a></li>
